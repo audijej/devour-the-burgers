2a2628a2e8fad8dc77def4358c26096b8bb9e7dd
Jesse Audije <audijej@gmail.com>
2020-07-14 21:45:21 -0700

    handlebars functioning with server and displaying html

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ("Mushroom Burger");
+INSERT INTO burgers (burger_name) VALUES ("Double Bacon Burger");
+INSERT INTO burgers (burger_name) VALUES ("Crispy Chicken Burger");
