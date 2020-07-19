13626b4082f82eb55486bdb1dffad45921bb0f8e
Jesse Audije <audijej@gmail.com>
2020-07-19 15:06:27 -0700

    handlebar partials rendering to page

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,5 @@
+USE burgers_db
+
 INSERT INTO burgers (burger_name) VALUES ("Mushroom Burger");
 INSERT INTO burgers (burger_name) VALUES ("Double Bacon Burger");
-INSERT INTO burgers (burger_name) VALUES ("Crispy Chicken Burger");
+INSERT INTO burgers (burger_name) VALUES ("Crispy Chicken Burger");s
--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,27 +1,85 @@
-$(function() {
+// $(function() {
 
-    $(".create-form").on("submit", function(event){
-        console.log("Hello");
-        event.preventDefault();
+//     $(".create-form").on("submit", function(event){
+//         console.log("Hello");
+//         event.preventDefault();
     
+//         let newBurger = {
+//             burger_name: $("#burger-box").val().trim(),
+//             devoured: 0
+//         };
+
+//         console.log(newBurger);
+    
+//         $.ajax("/api/burgers", {
+//             type: "POST",
+//             data: newBurger
+//         }).then(
+//             function(){
+//             console.log("created new burger");
+//             location.reload();
+//         }
+//         );
+//     });
+
+
+// })
+
+$(function() {
+    $(".change-devour").on("click", function(event) {
+        let id = $(this).data("id");
+        let newDevour = $(this).data("newdevour");
+        let newDevourState = {
+            devoured: newDevour
+        };
+
+        $.ajax("/api/burgers/" + id, {
+            type: "PUT",
+            data: newDevourState
+        }).then(
+            function() {
+                console.log("changed devour to", newDevour);
+                location.reload();
+            }
+        );
+    });
+
+    $(".create-form").on("submit", function(event) {
+        event.preventDefault();
         let newBurger = {
-            burger_name: $("#burger-box").val().trim(),
+            burger_name: $("#burgerBox").val().trim(),
             devoured: 0
         };
 
-        console.log(newBurger);
-    
+        console.log(newBurger)
+
         $.ajax("/api/burgers", {
             type: "POST",
             data: newBurger
         }).then(
-            function(){
-            console.log("created new burger");
-            location.reload();
-        }
+            function() {
+                console.log("created new burger");
+                location.reload();
+            }
         );
     });
 
+    $(".delete").on("click", function(event) {
+        event.preventDefault();
+        let id = $(this).data("id");
+        console.log(id)
+        
+        $.ajax("/api/burgers/" + id, {
+            type: "DELETE"
+        }).then(
+            function() {
+                console.log("deleted a burger", id);
+                location.reload();
+            }
+        );
 
+        let devourBurgerDiv = $("<h3>").text("Burger has been devoured");
+        $("#deletedBurgerDiv").append(devourBurgerDiv);
+    });
 })
-
+  
